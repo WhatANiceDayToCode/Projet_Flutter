068c059b61c3e6730ed48b870a9b3e2ed2fe6422
Blue <leanefelis@gmail.com>
2022-03-15 17:17:25 +0100

    changement de stratégie , maintenant les pages sont séparées

--- projet_flutter/lib/main.dart
+++ projet_flutter/lib/main.dart
@@ -1,84 +1,154 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(const MyApp());
+void main() {
+  runApp(const MyApp());
+}
+
+const String page1 = "Ajouter";
+const String page2 = "Afficher";
+const String page3 = "Rechercher";
+const String title = "Projet Flutter";
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  static const String _title = 'Projet Flutter';
-
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: _title,
-      home: const MyStatefulWidget(),
-      theme: ThemeData(
-        primarySwatch: Colors.green,
-      ),
+    return const MaterialApp(
+      title: title,
+      home: MyHomePage(title: title),
     );
   }
 }
 
-class MyStatefulWidget extends StatefulWidget {
-  const MyStatefulWidget({Key? key}) : super(key: key);
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+  final String title;
 
   @override
-  State<MyStatefulWidget> createState() => _MyStatefulWidgetState();
+  State<MyHomePage> createState() => _MyHomePageState();
 }
 
-class _MyStatefulWidgetState extends State<MyStatefulWidget> {
-  int _selectedIndex = 0;
-  static const TextStyle optionStyle =
-      TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
-  static const List<Widget> _widgetOptions = <Widget>[
-    Text(
-      'Index 0: Ajouter',
-      style: optionStyle,
-    ),
-    Text(
-      'Index 1: Afficher',
-      style: optionStyle,
-    ),
-    Text(
-      'Index 2: Rechercher',
-      style: optionStyle,
-    ),
-  ];
+class _MyHomePageState extends State<MyHomePage> {
+  late List<Widget> _pages;
+  late Widget _page1;
+  late Widget _page2;
+  late Widget _page3;
+  late int _currentIndex;
+  late Widget _currentPage;
 
-  void _onItemTapped(int index) {
+  @override
+  void initState() {
+    super.initState();
+    _page1 = const Page1();
+    _page2 = const Page2();
+    _page3 = Page3(changePage: _changeTab);
+    _pages = [_page1, _page2, _page3];
+    _currentIndex = 0;
+    _currentPage = _page1;
+  }
+
+  void _changeTab(int index) {
     setState(() {
-      _selectedIndex = index;
+      _currentIndex = index;
+      _currentPage = _pages[index];
     });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // Appbar
-      // appBar: AppBar(
-      //   title: const Text('Projet Flutter'),
-      // ),
-      body: Center(
-        child: _widgetOptions.elementAt(_selectedIndex),
+      appBar: AppBar(
+        title: Text(widget.title),
       ),
+      body: _currentPage,
       bottomNavigationBar: BottomNavigationBar(
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.edit),
-            label: 'Ajouter',
+          onTap: (index) {
+            _changeTab(index);
+          },
+          currentIndex: _currentIndex,
+          items: const [
+            BottomNavigationBarItem(
+              label: page1,
+              icon: Icon(Icons.edit),
+            ),
+            BottomNavigationBarItem(
+              label: page2,
+              icon: Icon(Icons.list),
+            ),
+            BottomNavigationBarItem(
+              label: page3,
+              icon: Icon(Icons.search),
+            ),
+          ]),
+      drawer: Drawer(
+        child: Container(
+          margin: const EdgeInsets.only(top: 20.0),
+          child: Column(
+            children: <Widget>[
+              _navigationItemListTitle(page1, 0),
+              _navigationItemListTitle(page2, 1),
+              _navigationItemListTitle(page3, 2),
+            ],
           ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.list),
-            label: 'Afficher',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.search),
-            label: 'Rechercher',
-          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _navigationItemListTitle(String title, int index) {
+    return ListTile(
+      title: Text(
+        '$title Page',
+        style: TextStyle(color: Colors.blue[400], fontSize: 22.0),
+      ),
+      onTap: () {
+        Navigator.pop(context);
+        _changeTab(index);
+      },
+    );
+  }
+}
+
+class Page1 extends StatelessWidget {
+  const Page1({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('$page1 Page', style: Theme.of(context).textTheme.headline6),
+    );
+  }
+}
+
+class Page2 extends StatelessWidget {
+  const Page2({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('$page2 Page', style: Theme.of(context).textTheme.headline6),
+    );
+  }
+}
+
+class Page3 extends StatelessWidget {
+  const Page3({Key? key, required this.changePage}) : super(key: key);
+  final void Function(int) changePage;
+
+  @override
+  Widget build(BuildContext context) {
+    return Align(
+      alignment: Alignment.center,
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Text('$page3 Page', style: Theme.of(context).textTheme.headline6),
+          ElevatedButton(
+            onPressed: () => changePage(0),
+            child: const Text('Switch to Home Page'),
+          )
         ],
-        currentIndex: _selectedIndex,
-        selectedItemColor: Colors.green[700],
-        onTap: _onItemTapped,
       ),
     );
   }
