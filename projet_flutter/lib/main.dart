320dd0d96558013e34f09fe0b34bbdf51581d962
Blue <leanefelis@gmail.com>
2022-03-28 15:02:37 +0200

    affichage page info lors de clic + possibilité retour

--- projet_flutter/lib/main.dart
+++ projet_flutter/lib/main.dart
@@ -23,9 +23,12 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       title: title,
       home: MyHomePage(title: title),
+      routes: {
+        '/afficher-infos': (context) => const Page3(),
+      },
     );
   }
 }
@@ -42,7 +45,6 @@ class _MyHomePageState extends State<MyHomePage> {
   late List<Widget> _pages;
   late Widget _page1;
   late Page2Widget _page2;
-  late Widget _page3;
   late int _currentIndex;
   late Widget _currentPage;
 
@@ -51,8 +53,7 @@ class _MyHomePageState extends State<MyHomePage> {
     super.initState();
     _page1 = const Page1();
     _page2 = Page2Widget();
-    _page3 = const Page3();
-    _pages = [_page1, _page2, _page3];
+    _pages = [_page1, _page2];
     _currentIndex = 0;
     _currentPage = _page1;
   }
@@ -86,39 +87,9 @@ class _MyHomePageState extends State<MyHomePage> {
               label: page2,
               icon: Icon(Icons.list),
             ),
-            // BottomNavigationBarItem(
-            //   label: page3,
-            //   icon: Icon(Icons.search),
-            // ),
           ]),
-      // Burger menu
-      // drawer: Drawer(
-      //   child: Container(
-      //     margin: const EdgeInsets.only(top: 20.0),
-      //     child: Column(
-      //       children: <Widget>[
-      //         _navigationItemListTitle(page1, 0),
-      //         _navigationItemListTitle(page2, 1),
-      //         // _navigationItemListTitle(page3, 2),
-      //       ],
-      //     ),
-      //   ),
-      // ),
     );
   }
-
-  // Widget _navigationItemListTitle(String title, int index) {
-  //   return ListTile(
-  //     title: Text(
-  //       '$title Page',
-  //       style: TextStyle(color: Colors.blue[400], fontSize: 22.0),
-  //     ),
-  //     onTap: () {
-  //       Navigator.pop(context);
-  //       _changeTab(index);
-  //     },
-  //   );
-  // }
 }
 
 class Page1 extends StatelessWidget {
@@ -137,8 +108,17 @@ class Page3 extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('$page1 Page', style: Theme.of(context).textTheme.headline6),
+    return Scaffold(
+      appBar: AppBar(title: const Text("Informations de [nom prénom]")),
+      body:
+          // Gestion du bouton back de l'appBar ET du téléphone
+          WillPopScope(
+        onWillPop: () async {
+          Navigator.pop(context);
+          return false;
+        },
+        child: Center(),
+      ),
     );
   }
 }
@@ -190,17 +170,11 @@ class Page2 extends State<Page2Widget> {
                     title: Text(_donnees[idx].nom),
                     subtitle: Text(_donnees[idx].prenom),
                     trailing: Text(_donnees[idx].telPort),
+                    onTap: () {
+                      //Navigator.pop(context);
+                      Navigator.pushNamed(context, '/afficher-infos');
+                    },
                   )
-                //   ListTile(
-                //     title: Text(_donnees![idx].libelle),
-                //     trailing: Icon(Icons.remove_circle,
-                //         subtitle: Text(_donnees![idx].libelle),
-                //         trailing: Text(_donnees![idx].libelle),
-                //         color: Theme.of(context).primaryColor),
-                //     onTap: () {
-                //       _showDialogSuppr(_donnees![idx]);
-                //     },
-                // ),
               ],
             ),
       floatingActionButton: FloatingActionButton(
@@ -227,7 +201,6 @@ class Page2 extends State<Page2Widget> {
               title: Text(person.nom),
               subtitle: Text(person.prenom),
               trailing: Text(person.telPort),
-              // trailing: Text('${person.age} ans'),
             ),
           ),
         ),
@@ -236,28 +209,3 @@ class Page2 extends State<Page2Widget> {
     );
   }
 }
-//   void _showDialogSuppr(Donnees categorieSelectionnee) {
-//     showDialog<String>(
-//       context: context,
-//       builder: (BuildContext context) => AlertDialog(
-//         title: const Text('Etes-vous sûr que vous voulez supprimer ?'),
-//         content: Text(categorieSelectionnee.libelle),
-//         actions: <Widget>[
-//           TextButton(
-//             onPressed: () => Navigator.pop(context, 'Cancel'),
-//             child: const Text('ANNULER'),
-//           ),
-//           TextButton(
-//             onPressed: () {
-//               setState(() {
-//                 _donnees!.remove(categorieSelectionnee);
-//               });
-//               Navigator.pop(context, 'OK');
-//             },
-//             child: const Text('OK'),
-//           ),
-//         ],
-//       ),
-//     );
-//   }
-// }
