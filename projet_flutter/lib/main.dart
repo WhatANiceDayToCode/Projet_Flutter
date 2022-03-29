3d87e71d0f53d3702dcacdaaf63fd0303f854edc
Blue <leanefelis@gmail.com>
2022-03-29 15:14:14 +0200

    Update main.dart

--- projet_flutter/lib/main.dart
+++ projet_flutter/lib/main.dart
@@ -22,7 +22,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       title: title,
       home: MyHomePage(title: title),
     );
